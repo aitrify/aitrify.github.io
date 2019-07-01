badcc475ddbc793e27d5e8b4813a334213fe485a
t-notch <t.notch@zoho.eu>
2019-07-01 19:33:40 +0200

    fix search

--- assets/js/search.js
+++ assets/js/search.js
@@ -7,7 +7,7 @@
 
       for (var i = 0; i < results.length; i++) {  // Iterate over the results
         var post = store[results[i].ref];        
-        appendString += '<li><a href="' + item.url + '"><h3>' + item.title+ '</h3></a>';
+        appendString += '<li><a href="' + post.url + '"><h3>' + post.title+ '</h3></a>';
         //appendString += '<div class="row listrecent">{% include postbox.html %}</div>';
       }
 
